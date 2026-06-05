858206d17df2e2f32677c10deeff223c97a22d93
ganesh <105532515+Gani7274@users.noreply.github.com>
2026-06-05 17:22:55 +0530

    Update HTML structure and add task2.js with new functionalities

--- new.js
+++ new.js
@@ -1,79 +1,79 @@
-let studentName = "Ganesh";
-let studentAge = 23;
-let studentCourse = "CSE";
+// let studentName = "Ganesh";
+// let studentAge = 23;
+// let studentCourse = "CSE";
 
-console.log(studentName);
-console.log(studentAge);
-console.log(studentCourse);
-
-
-
-var company = "Stackly";
-let salary = 20000;
-const country = "India";
-
-company = "Google"; 
-salary = 30000; 
-
-console.log(company);
-console.log(salary);
-console.log(country);
+// console.log(studentName);
+// console.log(studentAge);
+// console.log(studentCourse);
 
 
 
-let userName = prompt("What is your name?");
-console.log("Welcome " + userName);
+// var company = "Stackly";
+// let salary = 20000;
+// const country = "India";
+
+// company = "Google"; 
+// salary = 30000; 
+
+// console.log(company);
+// console.log(salary);
+// console.log(country);
 
 
 
-let knowsJS = confirm("Do you know JavaScript?");
-console.log(knowsJS);
+// let userName = prompt("What is your name?");
+// console.log("Welcome " + userName);
 
 
 
-let nameInput = prompt("Enter Student Name:");
-let ageInput = prompt("Enter Student Age:");
-let cityInput = prompt("Enter Student City:");
-
-console.log("Name : " + nameInput);
-console.log("Age : " + ageInput);
-console.log("City : " + cityInput);
+// let knowsJS = confirm("Do you know JavaScript?");
+// console.log(knowsJS);
 
 
 
-let course = "JavaScript";
-alert("Welcome to " + course);
+// let nameInput = prompt("Enter Student Name:");
+// let ageInput = prompt("Enter Student Age:");
+// let cityInput = prompt("Enter Student City:");
+
+// console.log("Name : " + nameInput);
+// console.log("Age : " + ageInput);
+// console.log("City : " + cityInput);
 
 
 
-document.writeln("Hello Student <br>");
-document.writeln("Welcome to JavaScript");
+// let course = "JavaScript";
+// alert("Welcome to " + course);
 
 
 
-let favoriteFood = "Chicken Biryani";
-let favoriteColor = "Yellow";
-let favoriteMovie = "Ragnarok";
-
-console.log("Food : " + favoriteFood);
-console.log("Color : " + favoriteColor);
-console.log("Movie : " + favoriteMovie);
+// document.writeln("Hello Student <br>");
+// document.writeln("Welcome to JavaScript");
 
 
 
-let name = "Ganesh";
-let age = 23;
-let phone = "7996772746";
-let email = "ganesh.rem74@gmail.com";
+// let favoriteFood = "Chicken Biryani";
+// let favoriteColor = "Yellow";
+// let favoriteMovie = "Ragnarok";
 
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("Phone:", phone);
-console.log("Email:", email);
-
-let challengeName = prompt("Enter your Name:");
-let challengeCourse = prompt("Enter your Course:");
+// console.log("Food : " + favoriteFood);
+// console.log("Color : " + favoriteColor);
+// console.log("Movie : " + favoriteMovie);
 
 
 
-alert("Hello " + challengeName + ", Welcome to " + challengeCourse + " Course");
+// let name = "Ganesh";
+// let age = 23;
+// let phone = "7996772746";
+// let email = "ganesh.rem74@gmail.com";
+
+// console.log("Name:", name);
+// console.log("Age:", age);
+// console.log("Phone:", phone);
+// console.log("Email:", email);
+
+// let challengeName = prompt("Enter your Name:");
+// let challengeCourse = prompt("Enter your Course:");
+
+
+
+// alert("Hello " + challengeName + ", Welcome to " + challengeCourse + " Course");
---
+++ task2.js
@@ -0,0 +1,151 @@
+let marks = 75;
+
+let status = marks >= 35 ? "Pass" : "Fail";
+console.log("Status: " + status);
+
+let grade;
+if (marks >= 90) {
+    grade = "Grade A";
+} else if (marks >= 75) {
+    grade = "Grade B";
+} else if (marks >= 60) {
+    grade = "Grade C";
+} else {
+    grade = "No Grade";
+}
+console.log(grade);
+
+
+
+let salary = 25000;
+
+let bonus = salary * 0.10; 
+let finalSalary = salary + bonus;
+
+console.log("Original Salary : " + salary);
+console.log("Bonus : " + bonus);
+console.log("Final Salary : " + finalSalary);
+
+
+
+let email = "admin@gmail.com";
+let password = "12345";
+
+if(email=="admin@gmail.com" & password=="12345"){
+    console.log("login success");
+}
+        
+else{
+    console.log("login failed");
+}
+
+
+
+// let price=2000;
+
+// let discount=price>1000?price*0.20:0;
+// let finalamount = price-discount;
+
+// console.log("final amount:"+finalamount);
+
+
+
+let products=["laptop","mobile","mouse","keyboard"];
+console.log("first product:"+products[0]);
+console.log("last product:"+products[products.length-1]);
+console.log("total products count:"+products.length);
+
+
+
+let employee={
+    name:"naveen",
+    department:"development",
+    salary:30000
+};
+console.log("all details:",employee);
+console.log("employee name:"+employee.name);
+console.log("salary:"+employee.salary);
+
+
+
+let shirt = 500;
+let pant = 1000;
+let shoe = 1500;
+
+let totalBill = shirt + pant + shoe;
+let discount = totalBill * 0.10;
+let finalAmount = totalBill - discount;
+
+console.log("Final Amount : " + finalAmount);
+
+
+
+
+let age=19;
+if(age>=18){
+    console.log("eligible for voting");
+}
+else{
+    console.log("not eligible");
+}
+
+
+
+
+let a=10;
+
+a++;
+console.log("after a++:" +a);
+
+++a;
+console.log("after ++a:"+a);
+
+a--;
+console.log("after a--:"+a);
+
+--a;
+console.log("after --a:"+a);
+
+
+
+
+let isPresent = true;
+let completedTask = true;
+
+if (isPresent && completedTask) {
+    console.log("Eligible for Salary");
+} else {
+    console.log("Not Eligible");
+}
+
+
+
+
+let companyEmployee = {
+    id: 101,
+    name: "John",
+    department: "Development",
+    salary: 35000,
+    experience: 2
+};
+
+
+let companyBonus = companyEmployee.salary * 0.10;
+let companyFinalSalary = companyEmployee.salary + companyBonus;
+let performanceStatus = companyEmployee.experience >= 2 ? "Good" : "Average";
+
+
+console.log("Employee ID : " + companyEmployee.id);
+console.log("Employee Name : " + companyEmployee.name);
+console.log("Department : " + companyEmployee.department);
+console.log("Salary : " + companyEmployee.salary);
+console.log("Experience : " + companyEmployee.experience + " Years");
+
+console.log("Bonus : " + companyBonus);
+console.log("Final Salary : " + companyFinalSalary);
+
+console.log("Performance Status : " + performanceStatus);
+
+
+
+        
